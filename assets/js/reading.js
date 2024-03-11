18970df7a7999f2c22b59441ea79af765a965da7
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-03-12 03:15:04 +0900

    google API検索窓

---
+++ assets/js/reading.js
@@ -0,0 +1,35 @@
+function searchBooks() {
+  var searchTerm = document.getElementById("searchTerm").value;
+  var url = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayResults(data.items))
+    .catch((error) => console.error("Error:", error));
+}
+
+function displayResults(results) {
+  var container = document.getElementById("searchResults");
+  container.innerHTML = ""; // 検索結果をクリア
+
+  if (results && results.length > 0) {
+    var ul = document.createElement("ul");
+    for (var i = 0; i < results.length; i++) {
+      var volumeInfo = results[i].volumeInfo;
+      var title = volumeInfo.title;
+      var authors = volumeInfo.authors
+        ? volumeInfo.authors.join(", ")
+        : "Unknown author";
+      var year = volumeInfo.publishedDate
+        ? volumeInfo.publishedDate.substr(0, 4)
+        : "Unknown year";
+
+      var li = document.createElement("li");
+      li.textContent = title + " by " + authors + ", published in " + year;
+      ul.appendChild(li);
+    }
+    container.appendChild(ul);
+  } else {
+    container.innerHTML = "<p>No results found</p>";
+  }
+}
