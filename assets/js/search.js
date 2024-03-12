b6c2896bff16b303cb66dd8f3edc7d524d96ce76
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-03-13 02:29:11 +0900

    タブベース作成

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,4 @@
+// 本の検索
 function searchBooks() {
   var searchTerm = document.getElementById("searchTerm").value;
   var url = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm;
@@ -38,3 +39,35 @@ function displayResults(results) {
     container.innerHTML = "<p>No results found</p>";
   }
 }
+
+// タブの作成
+document.addEventListener("DOMContentLoaded", function () {
+  // タブのリンクを取得
+  var tabLinks = document.querySelectorAll(".tab-links li a");
+
+  // タブのコンテンツを取得
+  var tabContents = document.querySelectorAll(".tab-content");
+
+  // タブリンクにイベントリスナーを追加
+  tabLinks.forEach(function (tabLink) {
+    tabLink.addEventListener("click", function (event) {
+      event.preventDefault(); // デフォルトのリンク動作を無効化
+
+      // タブの表示を切り替える
+      var targetTabId = this.getAttribute("href").substring(1);
+      tabContents.forEach(function (tabContent) {
+        if (tabContent.id === targetTabId) {
+          tabContent.classList.add("active");
+        } else {
+          tabContent.classList.remove("active");
+        }
+      });
+
+      // タブの選択状態を切り替える
+      tabLinks.forEach(function (link) {
+        link.classList.remove("active");
+      });
+      this.classList.add("active");
+    });
+  });
+});
