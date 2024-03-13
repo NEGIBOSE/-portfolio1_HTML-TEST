ffec27d92e8a7dfb6320f2def48c47a8b09d5727
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-03-13 14:48:51 +0900

    js移動

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,3 +17,45 @@ window.onload = function () {
   var img = document.getElementById("myImage");
   img.src = randomImage;
 };
+
+// 本の検索
+function searchBooks() {
+  var searchTerm = document.getElementById("searchTerm").value;
+  var url = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayResults(data.items))
+    .catch((error) => console.error("Error:", error));
+}
+
+function displayResults(results) {
+  var container = document.getElementById("searchResults");
+  container.innerHTML = ""; // 検索結果をクリア
+
+  if (results && results.length > 0) {
+    var ul = document.createElement("ul");
+    for (var i = 0; i < results.length; i++) {
+      var volumeInfo = results[i].volumeInfo;
+      var title = volumeInfo.title;
+      var authors = volumeInfo.authors ? volumeInfo.authors.join(", ") : "不明";
+      var year = volumeInfo.publishedDate
+        ? volumeInfo.publishedDate.substr(0, 4)
+        : "不明";
+
+      var li = document.createElement("li");
+      li.textContent = title + " /著 " + authors + " / " + year + "年";
+
+      // クリックしたタイトルを検索ボックスに自動入力する
+      li.addEventListener("click", function () {
+        document.getElementById("searchTerm").value =
+          this.textContent.split(" 著 ")[0];
+      });
+
+      ul.appendChild(li);
+    }
+    container.appendChild(ul);
+  } else {
+    container.innerHTML = "<p>No results found</p>";
+  }
+}
