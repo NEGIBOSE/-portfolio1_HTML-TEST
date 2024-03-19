02fc965ddda22625981c790e1477b2181edf6f72
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-03-20 02:44:03 +0900

    reading 数秒後にコメントを表示

--- assets/js/reading.js
+++ assets/js/reading.js
@@ -4,3 +4,12 @@ var selectedImageUrl = sessionStorage.getItem("selectedImageUrl");
 // 画像を表示する要素のIDを取得し、選択した本の画像のURLを設定
 var selectedImage = document.getElementById("selectedImage");
 selectedImage.src = selectedImageUrl;
+
+// ページの読み込みが完了したときに実行
+document.addEventListener("DOMContentLoaded", function () {
+  // 5秒後にコメントを表示する
+  setTimeout(function () {
+    var commentElement = document.getElementById("comment");
+    commentElement.style.display = "block"; // コメントを表示
+  }, 5000); // 5000ミリ秒（5秒）後に実行
+});
